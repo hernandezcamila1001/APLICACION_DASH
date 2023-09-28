02010da42e5379a167c7c272d2d46b65fbf4e5f2
MaritzaRf <143672262+MaritzaRf@users.noreply.github.com>
2023-09-27 22:10:47 -0500

    Update area3.py

--- frontend/area3.py
+++ frontend/area3.py
@@ -8,13 +8,7 @@ area3 = dbc.Container([
     # html.H3('Registro de afectaciones'), # Formato de Texto
     # html.Hr(),              # Agrega una linea abajo del titulo
     dbc.Row([
-        #dbc.Col('NOTICIAS', md=4, style={'background-color':'#1E90FF','text-align': 'center'}), # md= ancho del bloque, style= para qye se vea el color
         #dbc.Col('EMPRESA', md=4, style={'background-color':'#86BCFF','text-align': 'center'}),
-        #dbc.Col('CONOCENOS', md=4, style={'background-color':'#B3D3FF','text-align': 'center'}),
-        #html.Br(), 
-        #dbc.Col('.', md=12, style={'background-color':'white','text-align': 'center', 'text-color':'white'}),
-        # dbc.Button('BUSCAR',  className="ms-12",color="none",
-        #         style={"background": "linear-gradient(to right, #86BCFF, #F0FFFF)", 'text-align': 'center', 'width': '40%', 'outline':True}),
         dbc.Tabs(
             [
                 dbc.Tab(label="NOTICIAS", tab_id="noticias"),
